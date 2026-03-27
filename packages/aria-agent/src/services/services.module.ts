7b6186069560605b3dfe2553bcf70f4879acf452
NabilThange <thangenabil@gmail.com>
2026-03-28 00:44:14 +0530

    websokcet updat

--- packages/aria-agent/src/services/services.module.ts
+++ packages/aria-agent/src/services/services.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { WorkflowService } from './workflow.service';
 import { PinchTabService } from './pinchtab.service';
 import { DesktopService } from './desktop.service';
 import { BrowserLoggerService } from '../logger/browser-logger.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { MessagesModule } from '../messages/messages.module';
 
 @Module({
-  imports: [EventEmitterModule],
+  imports: [EventEmitterModule, forwardRef(() => MessagesModule)],
   providers: [WorkflowService, PinchTabService, DesktopService, BrowserLoggerService],
   exports: [WorkflowService, PinchTabService, DesktopService, BrowserLoggerService],
 })
--- packages/aria-agent/src/workflows/workflows.module.ts
+++ packages/aria-agent/src/workflows/workflows.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { WorkflowsController } from './workflows.controller';
 import { WorkflowService } from '../services/workflow.service';
 import { PinchTabService } from '../services/pinchtab.service';
 import { DesktopService } from '../services/desktop.service';
 import { BrowserLoggerService } from '../logger/browser-logger.service';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { MessagesModule } from '../messages/messages.module';
 
 @Module({
-  imports: [EventEmitterModule],
+  imports: [EventEmitterModule, forwardRef(() => MessagesModule)],
   controllers: [WorkflowsController],
   providers: [WorkflowService, PinchTabService, DesktopService, BrowserLoggerService],
   exports: [WorkflowService],
